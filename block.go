68b03773c47256d9a68fd6c97b76f2949cb61072
XuanGiaHanNguyen <han316@usf.edu>
2025-10-02 23:40:30 -0400

    created block struct and hashing mechanism

--- block.go
+++ block.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"strconv"
+)
+
+type Block struct { 
+	TimeStamp int64
+	Data []byte 
+	PrevDataHash []byte
+	Hash []byte
+}
+
+func (b *Block) setHash(){
+	timestamp := []byte(strconv.FormatInt(b.TimeStamp, 10))
+	headers := bytes.Join([][]byte{b.PrevDataHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+
+	b.Hash = hash[:]
+}
