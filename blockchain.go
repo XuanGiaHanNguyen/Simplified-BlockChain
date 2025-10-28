81341a8d381be64b4fa8115f09cec962187d9e10
XuanGiaHanNguyen <han316@usf.edu>
2025-10-28 13:23:29 -0400

    added cli for the block chain for user experience

--- blockchain.go
+++ blockchain.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-
 	bolt "go.etcd.io/bbolt"
 )
 
--- main.go
+++ main.go
@@ -1,23 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
-
 func main() {
 	bc := NewBlockChain()
 
-	bc.AddBlock("Testing Blockchain 1")
-	bc.AddBlock("Testing Blockchain 2")
+	defer bc.db.Close()
 
-	for _, block := range bc.blocks {
-		fmt.Printf("Prev. hash: %x\n", block.PrevDataHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Println()
-		pow := NewProofOfWork(block)
-		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-	}
+	cli := CLI{bc}
+	cli.Run()
 }
